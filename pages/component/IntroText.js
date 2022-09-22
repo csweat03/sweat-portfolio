c2cf3c0ffaa89a6beab0eff3efecf3b83f15a09b
Christian <sweatchristian03@gmail.com>
2022-09-22 17:14:25 -0400

    Added IntroText, Updated favicon

---
+++ pages/component/IntroText.js
@@ -0,0 +1,8 @@
+export default function IntroText() {
+  return (
+        <div className="intro-text">
+          <h1>Hi 👋, I'm Christian</h1>
+          <h3>A passionate full-stack developer from the United States.</h3>
+        </div>
+  );
+}
