4b24883010c8e2382294734cb9acc1b0d94dd177
Christian <sweatchristian03@gmail.com>
2022-09-24 21:05:09 -0400

    Cleaned up next.js sample, polished IntroText

--- pages/component/IntroText.js
+++ pages/component/IntroText.js
@@ -1,8 +1,30 @@
+const styles = {
+  h1: {
+    "letter-spacing": "0px",
+    "font-size": "3vw",
+    "margin": "2px"
+
+  },
+  h3: {
+    "letter-spacing": "1px",
+    "font-size": "1.2vw",
+    "margin": "2px"
+  },
+  accent: {
+    "color": "#0070f3"
+  },
+  introText: {
+    "margin": "auto",
+    "float": "left",
+    "padding": "40vh 6.25vw"
+  }
+}
+
 export default function IntroText() {
   return (
-        <div className="intro-text">
-          <h1>Hi 👋, I'm Christian</h1>
-          <h3>A passionate full-stack developer from the United States.</h3>
+        <div style={styles.introText}>
+          <h1 style={styles.h1}>Hi 👋, I'm <span style={styles.accent}>Christian Sweat</span></h1>
+          <h3 style={styles.h3}>A passionate full-stack developer from the United States.</h3>
         </div>
   );
 }
